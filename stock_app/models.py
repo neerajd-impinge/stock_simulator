1730ba460b0fb09d97fbe50429b472c7b50651ec
neeraj-impinge <neerajd.impinge@gmail.com>
2014-09-29 11:16:35 +0530

    signup form

--- stock_app/models.py
+++ stock_app/models.py
@@ -7,10 +7,7 @@ from django.contrib.auth.models import User
 
 
 
-class UserProfile(models.Model):
-	user=models.OneToOneField(User)
-	available_balance=models.DecimalField(max_digits=8,decimal_places=2,default=
-Decimal ( 100000 ) )
+
 
 
 # Create your models here.
