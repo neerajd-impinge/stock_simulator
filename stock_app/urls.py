cafbd7a7520110064723872df525941c9ab3ea14
neeraj-impinge <neerajd.impinge@gmail.com>
2014-09-29 18:37:04 +0530

    redirect updates

--- stock_app/urls.py
+++ stock_app/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.views import login
+from django.contrib.auth.forms import AuthenticationForm
+
 urlpatterns = patterns('',
-    url(r'^signup/', 'stock_app.views.auth'),
+    url(r'^signup/', 'stock_app.views.auth', name="signup" ),
     url(r'^login/', login,{'template_name':'login.html'}),
 	url(r'^search/', 'stock_app.views.search'),
 	url(r'^view/(?P<company>.+?)/', 'stock_app.views.viewCompany', name="view_stock"),
