fe184f73e8cbb6bb39c489ef5fff9723634a0a58
neeraj-impinge <neerajd.impinge@gmail.com>
2014-09-29 12:59:40 +0530

    redirect updates

--- stock_app/views.py
+++ stock_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render_to_response
-from django.http import HttpResponse, redirect
+from django.shortcuts import render_to_response, redirect
+from django.http import HttpResponse
 from django.template import RequestContext
 from django.contrib.auth.models import User
 
