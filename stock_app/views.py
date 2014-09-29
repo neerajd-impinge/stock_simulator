16b8a2af42bdef9d2fdb9b16fe5d8daabd0b2ea3
neeraj-impinge <neerajd.impinge@gmail.com>
2014-09-29 11:07:02 +0530

    signup form

--- stock_app/views.py
+++ stock_app/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.template import RequestContext
-
+from django.contrib.auth.models import User
 
 from django.contrib.auth.forms import UserCreationForm
 
 def	auth(request):
-	ci=RequestContext(request)
-	return render_to_response('sign_up.html',{'form':UserCreationForm()},ci)
+	form = UserCreationForm(request.POST or None)
+	if request.method == 'POST':
+		if form.is_valid():
+			data=form.cleaned_data
+			username=data['username']
+			password=data['password1']
+			User.objects.create_user(username=username,password=password)
+			return HttpResponseRedirect('/login')
+		return render_to_response('sign_up.html',{'form':form},ci)
+	else:
+		ci=RequestContext(request)
+		return render_to_response('sign_up.html',{'form':form},ci)
+	
+	
--- stocksimulator/settings.py
+++ stocksimulator/settings.py
@@ -27,6 +27,8 @@ TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = []
 
 
+LOGIN_REDIRECT_URL='/'
+
 # Application definition
 
 INSTALLED_APPS = (
