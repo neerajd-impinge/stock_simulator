8f1bff106206de8ba7bf06bcabd4b3a3523f2d25
neeraj-impinge <neerajd.impinge@gmail.com>
2014-09-29 10:51:31 +0530

    app updates

--- stock_app/views.py
+++ stock_app/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from django.template import RequestContext
+
+
+from django.contrib.auth.forms import UserCreationForm
 
 def	auth(request):
-	return HttpResponse("d")
+	ci=RequestContext(request)
+	return render_to_response('sign_up.html',{'form':UserCreationForm()},ci)
