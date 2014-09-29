ce837fb0f2cc1bd228b6cb1fb2c3eea1106c9a0c
neeraj-impinge <neerajd.impinge@gmail.com>
2014-09-29 11:21:13 +0530

    signup form

--- stock_app/urls.py
+++ stock_app/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
-from django.contrib.views import login
+from django.contrib.auth.views import login
 urlpatterns = patterns('',
     url(r'^signup/', 'stock_app.views.auth'),
-   # url(r'^login/', login,{'template_name':'login.html'}),
+    url(r'^login/', login,{'template_name':'login.html'}),
 )
